14b8095e8f1acc19aab2381e4574b651247f6e7a
bigrael123 <bigrael-x@hotmail.com>
2024-06-24 21:41:25 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+three_to_twelve = 3
+while(three_to_twelve <=12):
+    print(f"Listing numbers from 3 to 12: {three_to_twelve}")
+    three_to_twelve+=1
+for i in range(3, 13, 1):
+    print(f"Listing numbers from 3 to 12: {i}")
+zero_to_nine = 0
+while(zero_to_nine <=8):
+    print(f"Listing numbers from 0 to 8: {zero_to_nine}")
+    zero_to_nine+=2
+for i in range(0, 9, 2):
+    print(f"Listing numbers from 0 to 8: {i}")
